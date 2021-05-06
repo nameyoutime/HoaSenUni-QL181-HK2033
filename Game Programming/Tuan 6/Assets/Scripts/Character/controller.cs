cf988bb9afbb6e3876917309bfa17595f8b30055
Thien <bananakind123@gmail.com>
2021-05-06 18:57:05 +0700

    add w6

--- Game Programming/Tuan 6/Assets/Scripts/Character/controller.cs
+++ Game Programming/Tuan 6/Assets/Scripts/Character/controller.cs
@@ -4,55 +4,78 @@ using UnityEngine;
 
 public class controller : MonoBehaviour
 {
-    public movingBackground MovingBackground;
+    public movingBackground movingBackground;
     public Character2D character;
     public Animator animatior;
     private Rigidbody2D _rigidbody;
     public float runSpeed = 40f;
-    float horizontalMove = 0f, horizontalInput = 0f;
-    float direction;
+    private float horizontalMove, horizontalInput, direction;
     [SerializeField] private float m_JumpForce = 8f;
     [SerializeField] private GameObject bullet;
-
+    private bool jumped, isFalling;
 
     Object bulletref;
     // Start is called before the first frame update
     void Start()
     {
-
+        // jumped = false;isFalling = false;
         _rigidbody = GetComponent<Rigidbody2D>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        horizontalInput = Input.GetAxisRaw("Horizontal");
         animatior.SetFloat("Speed", Mathf.Abs(horizontalMove));
-        Jump();
-        Vector3 vel = _rigidbody.velocity;
-        if (vel.magnitude == 0)
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        move();
+        // Fire();
+
+    }
+    void checkStopMoving()
+    {
+        if (_rigidbody.velocity.x == 0 || horizontalInput == 0)
         {
-            MovingBackground.left = false;
-            MovingBackground.right = false;
+            movingBackground.left = false;
+            movingBackground.right = false;
             // do idle animations
         }
 
-        Fire();
-
     }
-    void Jump()
+    void move()
+    {
+        character.Move(horizontalMove * Time.fixedDeltaTime, horizontalInput);
+        falling();
+        jump();
+        checkStopMoving();
+    }
+    void falling()
+    {
+        if (_rigidbody.velocity.y < -0.1)
+        {
+            isFalling = true;
+            animatior.SetBool("isFalling", true);
+            animatior.SetBool("isJumping", false);
+
+        }
+    }
+    void jump()
     {
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
             _rigidbody.AddForce(new Vector2(0f, m_JumpForce), ForceMode2D.Impulse);
             animatior.SetBool("isJumping", true);
+            animatior.SetBool("isFalling", false);
+            jumped = true;
         }
         if (_rigidbody.velocity.y == 0)
         {
-            animatior.SetBool("isJumping", false);
+            jumped = false;
+            isFalling = false;
+            animatior.SetBool("isFalling", false);
         }
+
     }
 
     void Fire()
@@ -60,16 +83,6 @@ public class controller : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             Instantiate(bullet, transform.position, Quaternion.identity);
-
         }
-    }
-    void FixedUpdate()
-    {
-        character.Move(horizontalMove * Time.fixedDeltaTime, horizontalInput);
-    }
-
-    public void OnLanding()
-    {
-        animatior.SetBool("isJumping", false);
     }
 }
